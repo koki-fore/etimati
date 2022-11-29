761cf4c33210872af873b24e87ced8b9b99e48d5
koki-fore <furukunn@gmail.com>
2022-11-29 19:17:16 +0900

    余分な記述を消去

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,8 +41,8 @@ const Home = () => {
             <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
               <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
               <Box>
-                <Heading size='sm'>{post.user.screen_name}</Heading>
-                <Text color={theme.colors.accent}>{post.user.user_id}</Text>
+                <Heading color={theme.colors.accent} size='sm'>{post.user.screen_name}</Heading>
+                <Text >{post.user.user_id}</Text>
               </Box>
             </Flex>
           </Flex>
@@ -77,56 +77,6 @@ const Home = () => {
         </CardFooter>
       </Card>
     ))}
-    
-    <Card maxW='md'>
-    <CardHeader>
-      <Flex spacing='4'>
-        <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-          <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
-
-          <Box>
-            <Heading size='sm'>Segun Adebayo</Heading>
-            <Text>Creator, Chakra UI</Text>
-          </Box>
-        </Flex>
-        <IconButton
-          variant='ghost'
-          colorScheme='gray'
-          aria-label='See menu'
-          icon={<BsThreeDotsVertical />}
-        />
-      </Flex>
-    </CardHeader>
-    <CardBody>
-      <Text>
-        With Chakra UI, I wanted to sync the speed of development with the speed
-        of design. I wanted the developer to be just as excited as the designer to
-        create a screen.
-      </Text>
-    </CardBody>
-    <Image
-      objectFit='cover'
-      src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-      alt='Chakra UI'
-    />
-
-    <CardFooter
-      justify='space-between'
-      flexWrap='wrap'
-      sx={{
-        '& > button': {
-          minW: '136px',
-        },
-      }}
-    >
-      <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
-        いいね
-      </Button>
-      <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
-        コメント
-      </Button>
-    </CardFooter>
-  </Card>
   <Footer/>
   </Box>
   )
