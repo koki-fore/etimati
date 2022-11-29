82e4739c1b2ec1f5609c6b7137c3f05ee41c8c40
koki-fore <furukunn@gmail.com>
2022-11-29 22:31:57 +0900

    無駄な記述を消去

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import { useLayoutEffect, useState } from "react"
-import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, IconButton, Image, Text } from "@chakra-ui/react"
-import { BsThreeDotsVertical } from "react-icons/bs"
+import { Avatar, Box, Button, Card, CardBody, CardFooter, CardHeader, Flex, Heading, Image, Text } from "@chakra-ui/react"
 import { AiFillLike, AiOutlineLike} from "react-icons/ai"
-import { BiChat, BiLike } from "react-icons/bi"
+import { BiChat } from "react-icons/bi"
 // import { useAuthContext } from '../contexts/AuthContext';
 import axios from "axios";
 import theme from '../theme';
@@ -12,7 +11,6 @@ import Footer from '../components/Footer';
 
 const Home = () => {
   const [posts, setPosts] = useState()
-  const [usersMe, setUsersMe] = useState()
   const [active, setActive] = useState(false)
 
   const goodClick = () => {
@@ -30,25 +28,25 @@ const Home = () => {
       console.log(err)
     })
   },[])
-  if (!posts) return null
+  // if (!posts) return null
   return (
     <Box style={{textAlign: 'center',paddingTop:'4rem'}}>
     <Header/>
     {posts?.map((post) => (
       <Card maxW='md'>
         <CardHeader>
-          <Flex spacing='4'>
-            <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+          <Flex spacing='1'>
+            <Flex flex='4' gap='4' alignItems='center' flexWrap='wrap'>
               <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
               <Box>
-                <Heading color={theme.colors.accent} size='sm'>{post.user.screen_name}</Heading>
-                <Text >{post.user.user_id}</Text>
+                <Heading textAlign='left' size='sm'>{post.user.screen_name}</Heading>
+                <Text color={theme.colors.accent} textAlign='left'>{post.user.user_id}</Text>
               </Box>
             </Flex>
           </Flex>
         </CardHeader>
         <CardBody>
-          <Heading as='h3' size='lg' >水を3日分準備する</Heading>
+          <Heading as='h4' size='md' >{post.challenge.title}</Heading>
           <Text>
             {post.text}
           </Text>
