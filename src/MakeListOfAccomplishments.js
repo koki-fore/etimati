51a6833c3a9bf05daa42ffa84b1456054daaafc4
kip-i <kppisj@gmail.com>
2022-11-30 04:28:00 +0900

    feat: 場合分け

--- src/MakeListOfAccomplishments.js
+++ src/MakeListOfAccomplishments.js
@@ -1,12 +1,36 @@
-function MakeListOfAccomplishments(all, achievements){
-  let tureFalse = new Array(all.length).fill(false);
-  for(let i=0;i<achievements.length;i++){
-    tureFalse[achievements[i].id-1]=true;
+/*
+  status:statusを追加
+  only:達成したもののみ
+*/
+function MakeListOfAccomplishments(all, achievements,str){
+
+  switch (str){
+    case 'status':
+      let tureFalse = new Array(all.length).fill(false);
+      for(let i=0;i<achievements.length;i++){
+        tureFalse[achievements[i].id-1]=true;
+      }
+      for(let i=0;i<all.length;i++){
+        all[i].status=tureFalse[i];
+      }
+      return all;
+    case 'only':
+      console.log(all)
+      console.log(achievements)
+      let json=[];
+      for(let i=0;i<achievements.length;i++){
+        json.push({
+          title: all[Number(achievements[i].challenge_FK-1)].title,
+          text: all[achievements[i].challenge_FK-1].text,
+          experience_point: all[achievements[i].challenge_FK-1].experience_point,
+          id: all[achievements[i].challenge_FK-1].id
+        })
+      }
+      console.log(json)
+      return json;
+
+
   }
-  for(let i=0;i<all.length;i++){
-    all[i].status=tureFalse[i];
-  }
-  return all;
 }
 
 export default MakeListOfAccomplishments;
