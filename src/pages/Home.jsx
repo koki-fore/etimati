104418b5991e611bbaeb231a371d0b026e1a2681
kip-i <kppisj@gmail.com>
2022-12-02 00:15:32 +0900

    feat:footerに被らないようにした

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -59,57 +59,60 @@ const Home = () => {
   return (
     <Box style={{textAlign: 'center',paddingTop:'4rem'}}>
     <Header userInfo={userData}/>
-    {posts?.map((post) => (
-      <Card maxW='md'>
-        <CardHeader>
-          <Flex spacing='1'>
-            <Flex flex='4' gap='4' alignItems='center' flexWrap='wrap'>
-              <Avatar name='palpal' src={palpal} />
-              <Box>
-                <Heading textAlign='left' size='sm'>{post.user.screen_name}</Heading>
-                <Text color={theme.colors.accent} textAlign='left'>{post.user.user_id}</Text>
-              </Box>
+    <Box style={{paddingBottom:'70px'}}>
+      {posts?.map((post) => (
+        <Card maxW='md'>
+          <CardHeader>
+            <Flex spacing='1'>
+              <Flex flex='4' gap='4' alignItems='center' flexWrap='wrap'>
+                <Avatar name='palpal' src={palpal} />
+                <Box>
+                  <Heading textAlign='left' size='sm'>{post.user.screen_name}</Heading>
+                  <Text color={theme.colors.accent} textAlign='left'>{post.user.user_id}</Text>
+                </Box>
+              </Flex>
+              <Text>{post.created_at}</Text>
             </Flex>
-            <Text>{post.created_at}</Text>
-          </Flex>
-        </CardHeader>
-        <CardBody>
-          <Heading as='h4' size='md' style={{textAlign: 'center'}}>{post.challenge.title}</Heading>
-          <Text style={{textAlign: 'left'}}>
-            {post.text}
-          </Text>
-        </CardBody>
-        {/* <Image
-          objectFit='cover'
-          src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-          alt='Chakra UI'
-        /> */}
+          </CardHeader>
+          <CardBody>
+            <Heading as='h4' size='md' style={{textAlign: 'center'}}>{post.challenge.title}</Heading>
+            <Text style={{textAlign: 'left'}}>
+              {post.text}
+            </Text>
+          </CardBody>
+          {/* <Image
+            objectFit='cover'
+            src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            alt='Chakra UI'
+          /> */}
 
-        <CardFooter
-          justify='space-between'
-          flexWrap='wrap'
-          
-        >
-          <Button
-            onClick={onClickGood}
-            flex='1' 
-            variant='ghost' 
-            leftIcon={active ? <AiFillLike color={theme.colors.sub} /> : <AiOutlineLike  />}
-            _hover={{bg: 'none'}}
-            >
-            いいね
-          </Button>
-          <Button 
-            onClick={() => navigate('/commentList/'+post.id)}
-            flex='1' 
-            variant='ghost' 
-            _hover={{bg: 'none'}}
-            leftIcon={<BiChat />}>
-            コメント
-          </Button>
-        </CardFooter>
-      </Card>
-    ))}
+          <CardFooter
+            justify='space-between'
+            flexWrap='wrap'
+            
+          >
+            <Button
+              onClick={onClickGood}
+              flex='1' 
+              variant='ghost' 
+              leftIcon={active ? <AiFillLike color={theme.colors.sub} /> : <AiOutlineLike  />}
+              _hover={{bg: 'none'}}
+              >
+              いいね
+            </Button>
+            <Button 
+              onClick={() => navigate('/commentList/'+post.id)}
+              flex='1' 
+              variant='ghost' 
+              _hover={{bg: 'none'}}
+              leftIcon={<BiChat />}>
+              コメント
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </Box>
+    
   <Footer/>
   </Box>
   )
