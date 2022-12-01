84c1db5028c153c17459705334753468cac8d3d2
kip-i <kppisj@gmail.com>
2022-12-02 03:36:32 +0900

    feat: 切り捨て

--- src/experience2various.js
+++ src/experience2various.js
@@ -1,7 +1,7 @@
 function experience2various( totalExperience){
   /*固定値*/
   const perLevelup=30;
-  const level=(totalExperience/perLevelup)+1;
+  const level=(Math.floor(totalExperience/perLevelup))+1;
   const upto=(totalExperience%perLevelup);
   const avatar=level/2;
   return {level:level,upto:upto,avatar:avatar}
