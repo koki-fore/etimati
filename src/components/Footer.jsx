06aa201cc12d8288db0116c8b4b1fd57ebbd7d31
koki-fore <furukunn@gmail.com>
2022-12-03 04:34:34 +0900

    Footerボタン位置を変更

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,8 +30,8 @@ const Footer = () => {
   return (
     <Box bgColor={theme.colors.main} style={{...footerStyle}} pos='fixed' align={'center'}>
       <Grid  templateColumns='repeat(8, 1fr)'>
-        <GridItem as='button' height={'65px'} _hover={{bg: theme.colors.sub}} colSpan={2} onClick={() => navigate('/mypage')}><FaUser size={'2rem'} style={{margin:'0 auto'}} />マイページ</GridItem>
         <GridItem as='button' height={'65px'} _hover={{bg: theme.colors.sub}} colSpan={2} onClick={() => navigate('/')}><FaHome size={'2rem'} style={{margin:'0 auto'}} />ホーム</GridItem>
+        <GridItem as='button' height={'65px'} _hover={{bg: theme.colors.sub}} colSpan={2} onClick={() => navigate('/mypage')}><FaUser size={'2rem'} style={{margin:'0 auto'}} />マイページ</GridItem>
         <GridItem as='button' height={'65px'} _hover={{bg: theme.colors.sub}} colSpan={2} onClick={() => navigate('/taskList')}><FaList size={'2rem'} style={{margin:'0 auto'}} />チャレンジ</GridItem>
         <GridItem as='button' height={'65px'} _hover={{bg: theme.colors.sub}} colSpan={2} onClick={() => navigate('/postPage')}><FaCommentMedical size={'2rem'} style={{margin:'0 auto',transform:'scale(-1,1)'}} />投稿</GridItem>
       </Grid>
